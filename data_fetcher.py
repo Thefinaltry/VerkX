f9edeaa3131001598cd3c3ce0cd00f1feaabebc2
thefinaltry <finnurhugi@gmail.com>
2026-01-21 12:54:28 +0000

    add data_fetcher.py and update requirements.txt

---
+++ data_fetcher.py
@@ -0,0 +1,9 @@
+
+
+icelandic_tickers = ['ARION','BRIM','EIK','EIM','FESTI','HAGA','HAMP','HEIMAR',
+                     'ICEAIR','ICESEA','ISB','ISF','KALDA','KLAPP','KVIKA','NOVA',
+                     'OLGERD','REITIR','SIMINN','SJOVA','SKAGI','SKEL','SOLID','SVN','SYN']
+
+
+
+link = 'https://www.nasdaq.com/market-activity/stocks/{ticker}'
