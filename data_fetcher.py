e360ea4a89ee037de888db793ec790422355b20b
Thefinaltry <finnurhugi@gmail.com>
2026-01-22 16:07:34 +0000

    Byrja á gagnasöfnun

--- data_fetcher.py
+++ data_fetcher.py
@@ -1,9 +1,40 @@
 
+import yfinance as yf
+import tickers as tk
 
-icelandic_tickers = ['ARION','BRIM','EIK','EIM','FESTI','HAGA','HAMP','HEIMAR',
-                     'ICEAIR','ICESEA','ISB','ISF','KALDA','KLAPP','KVIKA','NOVA',
-                     'OLGERD','REITIR','SIMINN','SJOVA','SKAGI','SKEL','SOLID','SVN','SYN']
+def data_fetcher(ticker, period, interval):
+
+    """
+    Fetches stock data for a given ticker symbol using yfinance library.
+    Args:
+        ticker (str): The stock ticker symbol.
+    Returns:
+        pandas.DataFrame: DataFrame containing historical stock data.
+    """
+
+    stock = yf.Ticker(ticker).history(period=period, interval=interval)
+
+    return stock
+
+def market_data_fetcher(market, period="max", interval="1d"):
+
+    """
+    Fetches market data from a given market.
+        market (str): The market identifier -> "ICELAND"... etc
+        !! Currently only "ICELAND" is supported !!
+
+    Uses data_fetcher() to get the individual stock data.
+
+    Returns: dict : { ticker1: data1, ticker2: data2, ... }
+        data{x} -> pandas.DataFrame: DataFrame containing historical stock data.
+        
+    """
+
+    data = {}
+    for ticker in getattr(tk, market):
+        data[ticker] = data_fetcher(ticker, period=period, interval=interval)
+
+    return data
 
 
 
-link = 'https://www.nasdaq.com/market-activity/stocks/{ticker}'
---
+++ tickers.py
@@ -0,0 +1,8 @@
+### Lists of stock tickers ###
+
+
+# Icelandic stock tickers
+
+Icelandic = ['ARION','BRIM','EIK','EIM','FESTI','HAGA','HAMP','HEIMAR',
+            'ICEAIR','ICESEA','ISB','ISF','KALDA','KLAPP','KVIKA','NOVA',
+            'OLGERD','REITIR','SIMINN','SJOVA','SKAGI','SKEL','SOLID','SVN','SYN']
