477b332db9b415c143a1948abcc15e34125d423b
Thefinaltry <finnurhugi@gmail.com>
2026-01-25 21:11:36 +0000

    dsadsadass

--- main.py
+++ main.py
@@ -2,9 +2,19 @@
 
 import functions as f
 
+
+### Testing functions ###
 data = f.get_data(country='iceland', period='1mo', interval='1d')
-print(data)
 returns = f.portfolio_returns(data)
+
 print(returns)
+
+
+### main application logic would go here ###
+
+
+def main():
+    ...
+    pass
 
 
