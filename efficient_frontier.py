83725bf3d670c40781f2a00f64d7a4b66a8367a2
BirgirBragi <birgirbragi@gmail.com>
2026-03-01 12:24:40 +0000

    Cleaner code
    Seperated a lot of the code into functions to simplify the code

--- efficient_frontier.py
+++ efficient_frontier.py
@@ -6,8 +6,46 @@ import pandas as pd
 ### Testing functions ###
 
 def get_covariance_matrix(returns):
-    return None
+    cov_matrix = returns.cov()
+    cov_annual = cov_matrix * 252
+    cov_inv = pd.DataFrame(
+        np.linalg.inv(cov_annual.values),
+        index=cov_annual.index,
+        columns=cov_annual.columns
+    )
+    return cov_annual, cov_inv
 
+def calculate_min_var(returns:pd.DataFrame, yearly_returns:pd.DataFrame):
+    _, cov_inv = get_covariance_matrix(returns)
+    ones = pd.Series(1.0, index=cov_inv.index)
+    numerator = cov_inv @ ones
+    denominator = ones.T @ cov_inv @ ones
+    min_var_weights = numerator / denominator
+
+    
+    expected_return_of_min_var = yearly_returns.T @ min_var_weights
+    std_of_min_var = 1/np.sqrt(denominator)
+    return [min_var_weights, expected_return_of_min_var, std_of_min_var]
+
+def calculate_efficient_frontier(returns:pd.DataFrame,yearly_returns:pd.DataFrame):
+    cov_annual, cov_inv = get_covariance_matrix(returns)
+    _, expected_return_of_min_var, _ = calculate_min_var(returns, yearly_returns)
+
+    ones = pd.Series(1.0, index=cov_inv.index)
+    mu = yearly_returns.loc[cov_inv.index]
+    denominator = ones.T @ cov_inv @ ones
+
+    target_returns = np.arange(float(expected_return_of_min_var), 0.50 + 1e-12, 0.0025)
+    stds = []
+    weights = []
+
+    for i in target_returns:
+        w = ((i * ((cov_inv @ mu * (denominator))-((cov_inv @ ones) * (ones.T @ cov_inv @ mu)))) + ((cov_inv @ ones) * (mu.T @ cov_inv @ mu)) - ((cov_inv @ mu) * (mu.T @ cov_inv @ ones)))/((denominator * (mu.T @ cov_inv @ mu))-((ones.T @ cov_inv @ mu) * (mu.T @ cov_inv @ ones)))
+        weights.append(w)
+        std = np.sqrt(w @ cov_annual @ w.T)
+        stds.append(std)
+    
+    return target_returns, stds, weights
 
 def portfolio_weights(cov_matrix:pd.DataFrame, exp_returns:pd.Series, target_return:float):
     
--- main.py
+++ main.py
@@ -7,69 +7,28 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-
-
-### Testing functions ###
-data = gt.get_data(country='iceland', period='8y', interval='1d')
-returns = gt.portfolio_returns(data)
-yearly_returns = gt.cal_yearly_returns(returns)
-
-#returns.to_csv('returns.csv')
-
-cov_matrix = returns.cov()
-cov_annual = cov_matrix * 252
-cov_inv = pd.DataFrame(
-    np.linalg.inv(cov_annual.values),
-    index=cov_annual.index,
-    columns=cov_annual.columns
-)
-
-ones = pd.Series(1.0, index=cov_inv.index)
-numerator = cov_inv @ ones
-denominator = ones.T @ cov_inv @ ones
-min_var_weights = numerator / denominator
-#print(min_var_weights)
-#print("sum:", min_var_weights.sum())
-
-print(min_var_weights)
-expected_return_of_min_var = yearly_returns.T @ min_var_weights
-std_of_min_var = 1/np.sqrt(denominator)
-print("Expected return:",expected_return_of_min_var)
-print("Standard deviation:", std_of_min_var)
-print(sum(min_var_weights))
-
-#mu = yearly_returns.loc[cov_inv.index].to_numpy().reshape(-1, 1)
-mu = yearly_returns.loc[cov_inv.index]
-
-target_returns = np.arange(float(expected_return_of_min_var), 0.50 + 1e-12, 0.0025)
-stds = []
-
-for i in target_returns:
-    w = ((i * ((cov_inv @ mu * (denominator))-((cov_inv @ ones) * (ones.T @ cov_inv @ mu)))) + ((cov_inv @ ones) * (mu.T @ cov_inv @ mu)) - ((cov_inv @ mu) * (mu.T @ cov_inv @ ones)))/((denominator * (mu.T @ cov_inv @ mu))-((ones.T @ cov_inv @ mu) * (mu.T @ cov_inv @ ones)))
-    std = np.sqrt(w @ cov_annual @ w.T)
-    stds.append(std)
-
-plt.figure(figsize=(8,5))
-plt.plot(stds, target_returns)
-plt.xlabel("Volatility")
-plt.ylabel("Expected Return")
-plt.title("Efficient Frontier (Íslenski markaðurinn)")
-plt.grid(True)
-plt.show()
-
-
-
-'''
-for i in range(len(list(returns.keys()))):
-    print(list(returns.keys())[i],len(returns[list(returns.keys())[i]]))
-'''
-
-
 ### main application logic would go here ###
 
-
 def main():
-    ...
-    pass
+    ### Testing functions ###
+    data = gt.get_data(country='iceland', period='8y', interval='1d')
+    returns = gt.portfolio_returns(data)
+    yearly_returns = gt.cal_yearly_returns(returns)
+
+    #returns.to_csv('returns.csv')
+
+    target_returns, stds, weights = ef.calculate_efficient_frontier(returns,yearly_returns)
+
+
+    plt.figure(figsize=(8,5))
+    plt.plot(stds, target_returns)
+    plt.xlabel("Volatility")
+    plt.ylabel("Expected Return")
+    plt.title("Efficient Frontier (Íslenski markaðurinn)")
+    plt.grid(True)
+    plt.show()
+
+if __name__ == "__main__":
+    main()
 
 
